6ea2594373d0e423bff60b72e70e58162004f423
linjw16 <linjw16@tsinghua.org.cn>
2022-01-24 13:20:20 +0800

    modifying

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import itertools
 import logging
 import random
+import sys
 import os
 
 class TB(object):
@@ -18,24 +19,37 @@ class TB(object):
 	def model(self, din):
 		"""Model of FracTCAM"""
 		din_i = []
-		out_1 = 0xFFFF
+		out_1 = sys.maxsize % (1 << self.WIDTH)
 		for i in range(self.WIDTH):
 			din_i.append(din >> (i*self.DEPTH))
 			out_1 = out_1 & din_i[i]
+			self.log.debug("(out_1, din_i) = (%s,%s)" % (bin(out_1), bin(din_i[i])))
+			print("(out_1, din_i) = (%s,%s)" % (bin(out_1), bin(din_i[i])))
 		return out_1
 		
 
-def random_payload(length=8, min=0, max=0x100):
+def random_payload(times=1, length=8, min=0, max=0x100):
+	max = 0x100 if max > 0x100 else max
+	min = 0x0 if min < 0x0 else min
 	set = range(min, max)
-	rand_set = random.sample(set, length)
-	rand_set = [0xFF, 0xFF, 0xFF, rand_set]
-	return bytes(itertools.islice(itertools.cycle(rand_set), length))
-	
+	for i in range(times):
+		rand_set = random.sample(set, length)
+		yield bytes(itertools.islice(itertools.cycle(rand_set), length))
+
+
+def rand_payload(times=1, length=8):
+	max = 0x100
+	min = 0x0
+	set = range(min, max)
+	rand_set = random.shuffle(set)
+	for i in range(times):
+		yield itertools.islice(itertools.cycle(rand_set), length)
 
 
 if(__name__=="__main__"):
 	tb = TB()
-	for din in [random_payload(3,0,0x100) for i in range(4)]:
+	# for din in [random_payload(3, 0, 0x100) for i in range(4)]:
+	for din in random_payload(4, 3, 0, 0x100):
 		in_1 = int.from_bytes(din, byteorder='big')
 		print("Input: %X = %s" % (in_1, bin(in_1)))
 		out_1 = tb.model(in_1)
